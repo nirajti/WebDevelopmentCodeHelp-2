06af41b1f380be79499623b8f74ad44d21580748
Niraj Kumar Tiwari <101389654+nirajti@users.noreply.github.com>
2023-02-27 11:01:42 +0530

    Add files via upload

---
+++ Week 6 - DOM+MODERNJS/Class 2/index.js
@@ -0,0 +1,43 @@
+//  function eventFunction() {
+//     console.log('I have clicked on the document');
+
+// }
+
+// document.addEventListener('click', eventFunction);
+
+// document.removeEventListener('click', eventFunction);
+
+// const content = document.querySelector('#wrapper');
+
+// content.addEventListener('click',function(babbar){
+//     console.log(babbar);
+// })
+
+// let links = document.querySelectorAll('a');
+// let thirdLink = links[2];
+
+// thirdLink.addEventListener('click', function(event) {
+//     event.preventDefault();
+//     console.log('maza aaya na,acha lga');
+// })
+
+// let myDiv = document.createElement('div');
+
+// function paraStatus(event) {
+//     console.log('para' + event.target.textContent);
+// }
+
+// myDiv.addEventListener('click', paraStatus);
+
+// for(let i=1; i<=100; i++);
+// let newElement.textContent = 'This is para' + i;
+
+// myDiv.appendChild(newElement);
+
+// let element = document.querySelector('#wrapper');
+
+// element.addEventListener('click', function(event) {
+//     if(event.target.nodeName==='span') {
+//         console.log('span pr click lia hai' +event.target.textContent);
+//     }
+// });
