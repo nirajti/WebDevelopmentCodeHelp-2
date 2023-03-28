bd7dc5692e262b45ab91b489bd2b5ffe68f21a57
Niraj Kumar Tiwari <101389654+nirajti@users.noreply.github.com>
2023-03-28 14:46:13 +0530

    Update index.js

--- Javascript-Basics 1/JavascriptBasics/javascript class Revision code/javascript class 4/index.js
+++ Javascript-Basics 1/JavascriptBasics/javascript class Revision code/javascript class 4/index.js
@@ -1,8 +1,8 @@
-// console.log('Jai maa');
+console.log('Jai maa');
 
-// let a = 4;
-// let b = 'Niraj';
-// console.log(a,b);
+let a = 4;
+let b = 'Niraj';
+console.log(a,b);
 
 maa();
 function maa() {
@@ -26,63 +26,63 @@ jump;
 //Anoymouns function
 
 
-// let x =1;
-// x = 'a';
-// console.log(x);
+let x =1;
+x = 'a';
+console.log(x);
 
-// function sum(a,b) {
-// return a+b;
-// }
-// // console.log(sum(3,2));
-// console.log(sum(3,10,5));
+function sum(a,b) {
+return a+b;
+}
+// console.log(sum(3,2));
+console.log(sum(3,10,5));
 
-// function sum(a,b) {
-//     let total = 0;
-//      for(let value of arguments)
-//        total = total + value;
-//        return total;
-// }
-// let ans = sum(1,2,3,4,5,10);
-// console.log(ans);
+function sum(a,b) {
+    let total = 0;
+     for(let value of arguments)
+       total = total + value;
+       return total;
+}
+let ans = sum(1,2,3,4,5,10);
+console.log(ans);
 
 //rest parameter
-// function sum(num,value,...args) {
-//     console.log(args);
-// }
-// sum(1,2,3,4,5);
+function sum(num,value,...args) {
+    console.log(args);
+}
+sum(1,2,3,4,5);
 
 // default parametrs
-// function intereset(p,r=2,q=5) {
-//     return p*r*q/100;
-// }
-// console.log(intereset(100,10,));
+function intereset(p,r=2,q=5) {
+    return p*r*q/100;
+}
+console.log(intereset(100,10,));
 
 //Getter Setter
 //getter -> access properties
 //setter -> change or mutate properties
 
-// let person ={
-//     fName: 'Niraj',
-//     lName: 'Tiwari'
-// };
+let person ={
+    fName: 'Niraj',
+    lName: 'Tiwari'
+};
 
-// function fullName() {
-//     return `${person.fName} ${person.lName}`;
-// }
-// console.log(fullName);
+function fullName() {
+    return `${person.fName} ${person.lName}`;
+}
+console.log(fullName);
 
-// let person = {
-//     firstName = 'Niraj'
-//     lastName = 'babu',
-//     get fullName() {
-//         return `${person.firstName} ${person.lastName}`;
-//     };
-//     set fullName(value) {
-//         let parts = value.split('');
-//         this.firstName = parts(0);
-//         this.lastName = parts(1);
-//     }
-// };
+let person = {
+    firstName = 'Niraj'
+    lastName = 'babu',
+    get fullName() {
+        return `${person.firstName} ${person.lastName}`;
+    };
+    set fullName(value) {
+        let parts = value.split('');
+        this.firstName = parts(0);
+        this.lastName = parts(1);
+    }
+};
 
-// person.fullName = 'Rahul kumar';
-// console.log(person.fullName);
+person.fullName = 'Rahul kumar';
+console.log(person.fullName);
